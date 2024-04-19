495f29ac0342a890aa0918eb8c09152e22f6410f
Михаил <@DESKTOP-1RCLF6R>
2024-04-20 00:15:37 +0300

    Add project files.

---
+++ prak.10.variant/prak.10.variant.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+void printAgeCategory(int age)
+{
+    if (age < 14) {
+        cout << "дитина" << endl;
+    }
+    else if (age < 18) {
+        cout << "підліток" << endl;
+    }
+    else if (age < 100) {
+        cout << "дорослий" << endl;
+    }
+}
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int age;
+    cout << "введіть вік: ";
+    cin >> age;
+
+    printAgeCategory(age);
+
+    return 0;
+}
